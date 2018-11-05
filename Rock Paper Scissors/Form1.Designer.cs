72008fa02e60e21d9cfbb3793d622cc67eec721b
JFrost135y <33869660+JFrost135y@users.noreply.github.com>
2018-11-05 15:22:02 -0500

    Add project files.

---
+++ Rock Paper Scissors/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Rock_Paper_Scissors
+{
+    partial class frmRockPaperScissors
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRockPaperScissors));
+            this.picRock = new System.Windows.Forms.PictureBox();
+            this.picPaper = new System.Windows.Forms.PictureBox();
+            this.picScissors = new System.Windows.Forms.PictureBox();
+            this.txtComputerScore = new System.Windows.Forms.TextBox();
+            this.txtPlayerScore = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picRock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPaper)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picScissors)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picRock
+            // 
+            this.picRock.Image = ((System.Drawing.Image)(resources.GetObject("picRock.Image")));
+            this.picRock.Location = new System.Drawing.Point(80, 97);
+            this.picRock.Margin = new System.Windows.Forms.Padding(4);
+            this.picRock.Name = "picRock";
+            this.picRock.Size = new System.Drawing.Size(64, 64);
+            this.picRock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picRock.TabIndex = 0;
+            this.picRock.TabStop = false;
+            this.picRock.Click += new System.EventHandler(this.picRock_Click);
+            // 
+            // picPaper
+            // 
+            this.picPaper.Image = global::Rock_Paper_Scissors.Properties.Resources.paper;
+            this.picPaper.Location = new System.Drawing.Point(197, 97);
+            this.picPaper.Margin = new System.Windows.Forms.Padding(4);
+            this.picPaper.Name = "picPaper";
+            this.picPaper.Size = new System.Drawing.Size(64, 64);
+            this.picPaper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPaper.TabIndex = 1;
+            this.picPaper.TabStop = false;
+            this.picPaper.Click += new System.EventHandler(this.picPaper_Click);
+            // 
+            // picScissors
+            // 
+            this.picScissors.Image = ((System.Drawing.Image)(resources.GetObject("picScissors.Image")));
+            this.picScissors.Location = new System.Drawing.Point(319, 97);
+            this.picScissors.Margin = new System.Windows.Forms.Padding(4);
+            this.picScissors.Name = "picScissors";
+            this.picScissors.Size = new System.Drawing.Size(64, 64);
+            this.picScissors.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picScissors.TabIndex = 2;
+            this.picScissors.TabStop = false;
+            this.picScissors.Click += new System.EventHandler(this.picScissors_Click);
+            // 
+            // txtComputerScore
+            // 
+            this.txtComputerScore.Location = new System.Drawing.Point(100, 223);
+            this.txtComputerScore.Margin = new System.Windows.Forms.Padding(4);
+            this.txtComputerScore.Name = "txtComputerScore";
+            this.txtComputerScore.ReadOnly = true;
+            this.txtComputerScore.Size = new System.Drawing.Size(84, 22);
+            this.txtComputerScore.TabIndex = 8;
+            // 
+            // txtPlayerScore
+            // 
+            this.txtPlayerScore.Location = new System.Drawing.Point(281, 223);
+            this.txtPlayerScore.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPlayerScore.Name = "txtPlayerScore";
+            this.txtPlayerScore.ReadOnly = true;
+            this.txtPlayerScore.Size = new System.Drawing.Size(84, 22);
+            this.txtPlayerScore.TabIndex = 7;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.txtMessage.Location = new System.Drawing.Point(16, 15);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ReadOnly = true;
+            this.txtMessage.Size = new System.Drawing.Size(432, 54);
+            this.txtMessage.TabIndex = 5;
+            this.txtMessage.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 203);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Computer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(319, 203);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Player";
+            // 
+            // frmRockPaperScissors
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 286);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtPlayerScore);
+            this.Controls.Add(this.txtComputerScore);
+            this.Controls.Add(this.picScissors);
+            this.Controls.Add(this.picPaper);
+            this.Controls.Add(this.picRock);
+            this.Name = "frmRockPaperScissors";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rock Paper Scissors";
+            ((System.ComponentModel.ISupportInitialize)(this.picRock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPaper)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picScissors)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picRock;
+        private System.Windows.Forms.PictureBox picPaper;
+        private System.Windows.Forms.PictureBox picScissors;
+        private System.Windows.Forms.TextBox txtComputerScore;
+        private System.Windows.Forms.TextBox txtPlayerScore;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
